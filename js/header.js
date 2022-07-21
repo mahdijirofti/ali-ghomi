94ba5f936ee6ae39a4f29b612c24ed1797aa867b
Jiri <mohandes.jiri@gmail.com>
2022-07-21 17:14:35 +0430

    Fix

--- js/header.js
+++ js/header.js
@@ -6,3 +6,16 @@ jQuery(window).on("scroll", function() {
     jQuery(".new-home-header").removeClass("new-home-header-active");
   }
 });
+
+jQuery(function() { // Dropdown toggle
+  jQuery('.dropdown-toggle').click(function() { jQuery(this).next('.dropdown').slideToggle();
+  });
+
+  jQuery(document).click(function(e)
+  {
+    var target = e.target;
+    if (!jQuery(target).is('.dropdown-toggle') && !jQuery(target).parents().is('.dropdown-toggle'))
+//{ jQuery('.dropdown').hide(); }
+    { jQuery('.dropdown').slideUp(); }
+  });
+});
